bd1c9adf5873869a01b4a0c9378bbb1b0a65cea7
Liu WanJin <jinguo1129@gmail.com>
2020-09-10 19:13:22 +0800

    Update EBOOK

--- src/style/colors.js
+++ src/style/colors.js
@@ -1,12 +1,12 @@
 export default {
   white: '#FFF',
   colorRed: '#ea445a',
-  headerContainerDark: '#252525',
-  feedTitleText: '#747D8C',
+  headerContainerDark: '#282828',
+  feedTitleText: '#857E8C',
   dark_grey: '#747D8C',
   darkBackground: '#1a171b',
   placeholderTextColor: '#C2C2C2',
   tvchannelbackgroundColor: '#363437',
   activeTabColor: '#62636A',
-  inactiveTabColor: '#26272C',
+  inactiveTabColor: '#27272C',
 };
